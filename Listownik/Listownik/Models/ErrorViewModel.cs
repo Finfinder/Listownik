2095ec8fdba958843bff08f8256c60ea9c623e83
Rafał Trzepocki <rtrzepocki@gmail.com>
2024-06-04 01:25:11 +0200

    Initial Commit

---
+++ Listownik/Listownik/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Listownik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
