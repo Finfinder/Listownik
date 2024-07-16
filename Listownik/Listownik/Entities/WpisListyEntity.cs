f7a1708047bec250cd7e6543a35b548d421087d6
Rafał Trzepocki <rtrzepocki@gmail.com>
2024-07-17 00:29:51 +0200

    Dodanie klasy WpisListyEntity

---
+++ Listownik/Listownik/Entities/WpisListyEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Listownik.Entities
+{
+    public class WpisListyEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        public string Nazwa { get; set; } = default!;
+        public string? Opis { get; set; }
+        public int Ilosc { get; set; }
+        public byte[]? Ikona { get; set; }
+        public KategorieEntity Kategoria { get; set; } = default!;
+        public ListyEntity Lista { get; set; } = default!;
+
+    }
+}
