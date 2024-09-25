d42bf6a0196bf48e1bddf6b5b6b1549153b9e00e
Rafał Trzepocki <rtrzepocki@gmail.com>
2024-09-25 18:35:15 +0200

    Aktualizacja pakietu i wyłączenie Razor Pages
    Zaktualizowano wersję pakietu `Microsoft.EntityFrameworkCore.Tools` z `8.0.7` do `8.0.8` w pliku `Listownik.csproj`, co może być związane z poprawkami błędów lub nowymi funkcjonalnościami w nowszej wersji.
    
    Zakomentowano wywołanie `app.MapRazorPages();` w pliku `Program.cs`, co może wskazywać na tymczasowe wyłączenie obsługi stron Razor Pages w aplikacji.

--- Listownik/Listownik/Program.cs
+++ Listownik/Listownik/Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+//app.MapRazorPages();
 
 app.Run();
